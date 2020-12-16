974b2a45ebf30315772437914fd7a9696fd38605
harshrai654 <harshraitth2@gmail.com>
2020-12-16 23:45:24 +0530

    Constants moved to separate file

---
+++ Backend/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+    messages:{
+        DB_SUCCESS : "Database connection successfull!",
+        BANK_DOC_FAIL : "Error in accessing documents in Bank Collection!",
+        BANKS_REQ : "Banks list sent",
+    },
+    names:{
+        DB_NAME : "qmgm",
+        COLLECTION_NAME : "Bank"
+    }
+};
